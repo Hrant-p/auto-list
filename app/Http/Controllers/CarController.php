9fcd01e5f53701b1986134c3c4bffcf2614c307e
Hrant-p <hrant@live.ru>
2020-03-02 22:39:38 +0400

    create Car model, controller, and migrating Car tables

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    public function getCars () {
+
+    }
+
+    public function showCar() {
+
+    }
+
+    public function addCar() {
+
+    }
+
+    public function deleteCar() {
+
+    }
+
+    public function updateCar() {
+
+    }
+}
